2d4222de618d2627fd17298c96d546a5cdb7d8f8
AhmedAsafrah <211033@ppu.edu.ps>
2025-06-11 00:21:47 +0200

    Fix CORS configuration: remove trailing slash from Vercel app origin URL

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ["/^http:\/\/localhost:\d+$/","https://ppu-clubs-git-main-mohammadoqailis-projects.vercel.app/"],
+    origin: ["/^http:\/\/localhost:\d+$/","https://ppu-clubs-git-main-mohammadoqailis-projects.vercel.app"],
     credentials: true,
   })
 );
